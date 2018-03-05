76fe3f23460862d254c4b271b0d9f1c116067070
Emma Garland <emma.garland@rocksolidknowledge.com>
2018-03-05 01:34:56 +0000

    DI for Umbraco backoffice to work with views

--- eg.skrift.data/CMS/Global.cs
+++ eg.skrift.data/CMS/Global.cs
@@ -27,7 +27,10 @@ namespace eg.skrift.data.CMS
             var builder = new ContainerBuilder();
             builder.RegisterInstance(ApplicationContext.Current).AsSelf();
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterControllers(typeof(UmbracoApplication).Assembly);
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(Global).Assembly);
             builder.RegisterType<UmbracoLoggerFactory>()
                 .As<ILoggerFactory>();
             builder.RegisterType<UmbracoContentFetcherFactory>()
